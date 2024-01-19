dd0a2c5d3cb87f1a93f85f861e154cd215339ad0
XeNn <48121436+rishi438@users.noreply.github.com>
2024-01-19 12:07:43 +0530

    Update environment.py

--- backend/utils/environment.py
+++ backend/utils/environment.py
@@ -21,7 +21,7 @@ MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
 MAIL_PORT = os.environ.get("MAIL_PORT", 587)
 MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", True)
 MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "ranjinijamun@gmail.com")
-MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", "greg tqho xtij egab")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", ************************)
 MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "ranjinijamun@gmail.com")
 
 # Celery
