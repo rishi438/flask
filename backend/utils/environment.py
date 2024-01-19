3882353f348251433495d67f51de797243dbc306
XeNn <48121436+rishi438@users.noreply.github.com>
2024-01-19 12:16:13 +0530

    Update environment.py

--- backend/utils/environment.py
+++ backend/utils/environment.py
@@ -20,9 +20,9 @@ CELERYBEAT_SCHEDULE_FILENAME = os.environ.get(
 MAIL_SERVER = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
 MAIL_PORT = os.environ.get("MAIL_PORT", 587)
 MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", True)
-MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "ranjinijamun@gmail.com")
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME", "********************")
 MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD", ************************)
-MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "ranjinijamun@gmail.com")
+MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "****************")
 
 # Celery
 CELERY_RABBITMQ_BROKER = os.environ.get(
